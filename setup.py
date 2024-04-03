aedf1f01fa6eae49328b6eba69525c800285fb0c
aa_ron_su <suaaron55@gmail.com>
2024-04-03 11:57:44 -0500

    first commit with new root

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BoXHED_Fuse',
+    version='1.0.0',
+    description='',
+    author='Aaron Su',
+    author_email='aa_ron_su@tamu.edu',
+    packages=find_packages(),
+)
